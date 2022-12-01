712cb68fad122f8d66f1c66ec5a0f19cd32bfa4c
用户名 <>
2022-12-01 20:13:15 +0800

    完成初始化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+import "normalize.css/normalize.css";
+const app = createApp(App);
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component);
+}
+app.use(store).use(router).mount("#app");
